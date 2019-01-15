9cc595e49e2689e2c5ba6b7743710ab09538c5e2
pasha <pashtet5246@gmail.com>
2019-01-15 16:23:55 +0700

    Fix dist folder

---
+++ docs/precache-manifest.face6f4c3c724de7977cbf5d0875f259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "44386d63f68e28f8a056",
+    "url": "/js/chunk-vendors.ea49da7c.js"
+  },
+  {
+    "revision": "715d990039f94e12c00d",
+    "url": "/js/app.3c7d0b33.js"
+  },
+  {
+    "revision": "51381fb9d0b3e55298b80d010c87df63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44386d63f68e28f8a056",
+    "url": "/css/chunk-vendors.2499bce8.css"
+  },
+  {
+    "revision": "715d990039f94e12c00d",
+    "url": "/css/app.983752b8.css"
+  }
+];
